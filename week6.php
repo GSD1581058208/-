e65e8429ff267a9f2a9db8829cd375a26d53b8b5
GSD1581058208 <1581058208@qq.com>
2018-11-14 10:59:46 +0800

    11.14

---
+++ week6.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dell
+ * Date: 2018/11/14
+ * Time: 8:24
+ */
+
+echo add(1,2);
+function Add($num1,$num2){
+        return array_sum([$num1,$num2]);
+    }

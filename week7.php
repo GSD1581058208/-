86b53aa5fa1873a04ee2fa68e9e099cb8703db67
GSD1581058208 <1581058208@qq.com>
2018-11-16 10:12:47 +0800

    11.14

---
+++ week7.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dell
+ * Date: 2018/11/15
+ * Time: 8:25
+ */
+$array = array[1,2,3,4,5,6,7,8,9];
+$sun = 17;
+function FindNumbersWithSum($array,$sum){
+    if (array_sum[$i,$j] == $sum){
+        echo $i."和".$j;
+    }
+}
